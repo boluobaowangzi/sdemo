791da222cc0aade0c97326f68dd2c17f9dfac92e
zhanghq <zhanghq@ushareit.com>
2022-06-12 17:16:26 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+  public static void main(String[] args) {
+
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+
+    Book book = applicationContext.getBean("book", Book.class);
+    book.printBookInfo();
+
+    Author author = applicationContext.getBean("author", Author.class);
+    author.printAuthorInfo();
+  }
+}
+
